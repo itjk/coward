a963b76a8945edc64e728ca0f74dd08410d39442
MikuroXina <ryosukadnak@gmail.com>
2020-07-30 02:34:22 +0900

    :zap: Improve EventHandler

---
+++ src/network/gateway/event/EventHandler.ts
@@ -0,0 +1,54 @@
+import { Client } from "../../../Client.ts";
+import { Payload } from "../Payload.ts";
+import { Emitter } from "../../../util/Emitter.ts";
+import { handleChannelEvent, RoleEventSubscriber } from "./handler/Channel.ts";
+import { handleGuildEvent, GuildEventSubscriber } from "./handler/Guild.ts";
+import {
+  handleMessageEvent,
+  MessageEventSubscriber,
+} from "./handler/Message.ts";
+import { GuildDB, ChannelDB } from "../event.ts";
+
+export interface EventSubscriber
+  extends RoleEventSubscriber, GuildEventSubscriber, MessageEventSubscriber {
+  ready: Emitter<{ type: "READY" }>;
+}
+
+export function handleEvent(
+  client: Client,
+  message: Payload,
+  subscriber: EventSubscriber,
+  database: GuildDB & ChannelDB,
+) {
+  if (message.t.startsWith("CHANNEL_")) {
+    handleChannelEvent(
+      message,
+      subscriber,
+      database,
+      client,
+    );
+    return;
+  }
+  if (message.t.startsWith("GUILD_")) {
+    handleGuildEvent(
+      client,
+      message,
+      subscriber,
+      database,
+    );
+    return;
+  }
+  if (message.t.startsWith("MESSAGE_")) {
+    handleMessageEvent(client, message, subscriber);
+    return;
+  }
+  const type = message.t;
+  switch (type) {
+    case "READY": {
+      subscriber.ready.emit({ type });
+      return;
+    }
+      // TODO: invites
+      // TODO: TYPING_START
+  }
+}
