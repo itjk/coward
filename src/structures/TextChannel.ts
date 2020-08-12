66da3586e879c2a34c72f271c93a9757ed827862
MikuroXina <ryosukadnak@gmail.com>
2020-08-12 18:03:45 +0900

    :pencil: Write docs for TextChannelMixIn

--- src/structures/TextChannel.ts
+++ src/structures/TextChannel.ts
@@ -19,18 +19,22 @@ export function TextChannelMixIn<T extends new (...args: any[]) => TextChannel>(
   Base: T,
 ) {
   return class extends Base {
+    /** Post a message in a channel. Requires the `SEND_MESSAGES` permission.*/
     createMessage(content: string | CreateMessage) {
       return this._messages.createMessage(this.id, content);
     }
 
+    /** Modify a message. Must be authored by you. */
     modifyMessage(messageID: string, content: string | ModifyMessage) {
       return this._messages.modifyMessage(this.id, messageID, content);
     }
 
+    /** Delete a message in a channel. Requires the `MANAGE_MESSAGES` permission. */
     deleteMessage(messageID: string) {
       return this._messages.deleteMessage(this.id, messageID);
     }
 
+    /** Show typing indicator while awaiting `func` Promise. */
     async withTyping(func: () => Promise<void>) {
       // `Trigger Typing Indicator` expires after 10 seconds
       const triggerTimer = setInterval(() => {
